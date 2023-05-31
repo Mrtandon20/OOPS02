1ff1b9ac1d29786cc6bc1dcf986c7c331435edbf
Varun Tandon <75672315+Mrtandon20@users.noreply.github.com>
2023-05-31 12:12:38 +0530

    Create Vehicle.java

---
+++ Components of OOPS/Vehicle.java
@@ -0,0 +1,12 @@
+public class Vehicle{
+
+    String color;
+    int maxspeed;
+
+    void print(){
+        System.out.println("Color Of Vehicle : " + color);
+        System.out.println("Maxspeed of Vehicle : " + maxspeed);
+    }
+
+
+}
