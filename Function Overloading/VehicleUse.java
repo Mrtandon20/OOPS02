f16fd1e12759ea624509161584827e862a81d438
Varun Tandon <75672315+Mrtandon20@users.noreply.github.com>
2023-05-31 12:34:30 +0530

    Create VehicleUse.java

---
+++ Function Overloading/VehicleUse.java
@@ -0,0 +1,19 @@
+public class VehicleUse {
+    public static void main(String[] args) {
+        Vehicle v1 = new Vehicle();
+        // v1.maxspeed= 100;
+        v1.setMaxspeed(100);
+        v1.color = "Yellow";
+        v1.print();
+
+        Car c1 = new Car();
+        c1.isconvertable = true;
+        c1.color ="Black";
+        c1.setMaxspeed(200);
+        c1.numGears = 5;
+
+        c1.print();
+
+    }
+    
+}
