021732155ac0353a2610978b126d5273c03ce8ad
Varun Tandon <75672315+Mrtandon20@users.noreply.github.com>
2023-05-31 12:21:21 +0530

    Create Car.java

---
+++ Inheritance & Private Members/Car.java
@@ -0,0 +1,12 @@
+public class Car extends Vehicle {
+    
+    boolean isconvertable;
+    int numGears;
+    
+    void Carprint(){
+        System.out.println("Color Of Car : " + color);
+        System.out.println("Maxspeed of Car : " + getMaxspeed());
+        System.out.println("Car isConvertable : " + isconvertable);
+        System.out.println("No Of Gears in Car : " + numGears);
+    }
+}
