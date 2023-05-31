6586fd84240cf1a7ffd74ee727287c17bd2c0f9d
Varun Tandon <75672315+Mrtandon20@users.noreply.github.com>
2023-05-31 12:13:36 +0530

    Create VehicleUse.java

---
+++ Components of OOPS/VehicleUse.java
@@ -0,0 +1,12 @@
+public class VehicleUse {
+    public static void main(String[] args) {
+        Vehicle v1 = new Vehicle();
+        v1.print();
+
+        Car c1 = new Car();
+        c1.isconvertable = true;
+        c1.color ="Black";
+        c1.print();
+    }
+    
+}
